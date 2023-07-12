8b08a5383fd657403b53140f2459aba844797cdf
mr-dargi <dargimohammadreza@gmail.com>
2023-07-12 11:04:56 +0330

    Making component for each section of our simple app in app.js getting style to it and I will separate them make separate component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
 import './App.css';
 
+function Header() {
+  return (
+      <header className="header">
+        <nav className="nav-bar">
+          <img className="logo-img" src="./react-logo.png" alt="logo" />
+          <ul className="list-item">
+            <li><a className="nav-link" href="#">Pricing</a></li>
+            <li><a className="nav-link" href="#">About</a></li>
+            <li><a className="nav-link" href="#">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+  );
+};
+
+function Footer() {
+  return (
+    <footer>
+      © 2023 mr.d development. All rights reserved.
+    </footer>
+  )
+}
+
+function MainContent() {
+  return (
+    <div>
+      <h1>The reasons I'm excited to learn react:</h1>
+      <ol>
+        <li>
+          It's a popular library, so I'll be 
+          able to fit in with the cool kids!
+        </li>
+        <li>
+          I'm more likely to get a job as a developer
+          if I know React
+        </li>
+      </ol>
+    </div>
+  );
+};
+
+
 function App() {
   return (
     <div className="App">
-      <img id="logoImg" src="./react-logo.png" alt="logo" />
-      <h1>Fun fact about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100k stars on Github</li>
-        <li>Is maintained by Facebook</li>
-        <li>Power thousends of enterprise apps, including mobile apps</li>
-      </ul>
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
